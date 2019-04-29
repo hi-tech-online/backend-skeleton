8f893a0f7154fc868e406ae8e0287f6db0ee76c6
zhoulin2 <zhoulin2@asiainfo.com>
2019-04-29 22:51:16 +0800

    重构

---
+++ src/main/java/com/hitech/skeleton/framework/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.hitech.skeleton.framework.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * WEB 通用配置
+ *
+ * @author Steven
+ * @date 2019-04-29
+ */
+@Configuration
+public class WebConfig {
+
+
+
+}
